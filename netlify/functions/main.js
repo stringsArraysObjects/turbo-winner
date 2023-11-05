4cc14f86dbcb9d11deb6785c3b8b7850bcd4c9ec
Mason <128002915+grandpaCanCode@users.noreply.github.com>
2023-11-05 00:15:01 -0400

    Update main.js

--- netlify/functions/main.js
+++ netlify/functions/main.js
@@ -1,7 +1,7 @@
-export async function handler() {
-    return {
-      statusCode: 200,
-      body: "server.js"
-    };
-  }
-  
+exports.handler = async () => {
+  const mySecret = process.env.MONGODB_URI;
+  return {
+    statusCode: 200,
+    body: `server.js ${mySecret}`
+  };
+};
